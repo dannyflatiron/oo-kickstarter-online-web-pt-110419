3fd501c5edfd50772a7fa8d74918285abe9f7032
Daniel Reina <dannyflatiron@gmail.com>
2019-12-18 19:06:17 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,7 +9,9 @@ attr_reader :name, :backed_projects
   end
 
   def back_project(project)
-    @backed_projects << project 
+    @backed_projects << project
+    # project represents an instance of Project because in the spec folder Project.new is already activated
+    #so we can use the argument project to call on the array .backers
     project.backers << self
     # binding.pry
   end
