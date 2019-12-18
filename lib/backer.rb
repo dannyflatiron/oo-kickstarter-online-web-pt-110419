2edc900a3cff547ff3759d9d141fe126d6bb71b7
Daniel Reina <dannyflatiron@gmail.com>
2019-12-18 17:51:40 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,7 +9,7 @@ attr_reader :name, :backed_projects
 
   def back_project(project)
     @backed_projects << project
-    @backed_projects << Project.backers
+    # @backed_projects << Project.backers
   end
 
 
