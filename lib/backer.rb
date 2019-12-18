ab0eae9c6be9f601118e5c4cf2d0525f92d6008a
Daniel Reina <dannyflatiron@gmail.com>
2019-12-18 19:00:44 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -10,9 +10,8 @@ attr_reader :name, :backed_projects
 
   def back_project(project)
     @backed_projects << project 
-
+    project.backers << self
     # binding.pry
-    #  From Project you want to add this current project to the backed_projects array, and also add it into project.backers to create the association
   end
 
 
